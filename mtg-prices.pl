f07d04c24aa66e68b3e20516376d280356570eeb
Tomasz Golinski <tomaszg@math.uwb.edu.pl>
2017-08-25 23:41:38 +0200

    MTG: mtg-prices.pl - script to list prices of all cards in the collection

---
+++ mtg-prices.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/env perl
+use Archive::Zip qw( :ERROR_CODES :CONSTANTS );
+
+use mtg;
+
+use Data::Dumper;
+
+%karty = mtg::read_base;
+$i = 0;
+# my $pricelist = {};
+
+foreach $id (keys %karty) {
+	$entry = mtg::get_entry($id);
+
+	if ($entry->{rare} eq 'Basic Land') { 
+		next;
+	}
+
+	$pricelist->{$id}->{price} = mtg::get_price($id,$entry->{title},$entry->{exp});
+	$pricelist->{$id}->{title} = $entry->{title};
+	$pricelist->{$id}->{title} =~ s/%2F/\//g; 
+	$pricelist->{$id}->{exp} = $entry->{exp};
+	$i++;
+# 	print $i." $entry->{title} ".$pricelist->{$id}->{price}."\n";
+# 	last if $i>=100;
+}
+
+# print Dumper($pricelist);
+
+foreach $id (sort { $pricelist->{$b}->{price} <=> $pricelist->{$a}->{price} } keys %$pricelist) {
+# foreach $id (keys %$pricelist) {
+    print $id.": ".$pricelist->{$id}->{title}." (".$pricelist->{$id}->{exp}."): ".$pricelist->{$id}->{price}."\n";
+}
