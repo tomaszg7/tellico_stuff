ef089871b1de2e9dadc5dedf76780dccac42e341
Tomasz Golinski <tomaszg@math.uwb.edu.pl>
2018-03-11 14:08:46 +0100

    MTG: mtg-prices.pl supports -n and -N to limit searching for one expansion only

--- mtg-prices.pl
+++ mtg-prices.pl
@@ -1,18 +1,45 @@
 #!/usr/bin/env perl
 use Archive::Zip qw( :ERROR_CODES :CONSTANTS );
+use Getopt::Std;
 
 use mtg;
 
-use Data::Dumper;
+getopts('N:n:h', \%opts);
+
+if ($opts{'h'}) {
+	die "-n: search by expansion, -N: seach by expansion list\n";
+}
+
+if ($opts{'l'}) {
+	mtg::list_exp;
+	exit;
+}
 
 %karty = mtg::read_base;
 $i = 0;
 $total = 0;
 
+if ($opts{'N'}) { 
+	my @tmp = split /[ ,]/, $opts{'N'}; 
+	%exps = map { $_, 1 } @tmp;
+}
+
+if ($opts{'n'}) {
+	$exps{$opts{'n'}} = 1;
+}
+
+foreach $exp (keys %exps) {
+	if ($mtg::expansions{ $exp }) {
+		delete $exps{$exp};
+		$exps{$mtg::expansions{ $exp }}=1;
+	}
+}
+
 foreach $id (keys %karty) {
 	$entry = mtg::get_entry($id);
 
-	if ($entry->{rare} eq 'Basic Land') { 
+	if (($entry->{rare} eq 'Basic Land') or (%exps and (! exists $exps{$entry->{exp}} ) ) ) { 
+#		print "skipping $entry->{title}\n";
 		next;
 	}
 
@@ -25,8 +52,6 @@ foreach $id (keys %karty) {
 # 	print $i." $entry->{title} ".$pricelist->{$id}->{price}."\n";
 # 	last if $i>=100;
 }
-
-# print Dumper($pricelist);
 
 foreach $id (sort { $pricelist->{$b}->{price} <=> $pricelist->{$a}->{price} } keys %$pricelist) {
     print $id.": ".$pricelist->{$id}->{title}." (".$pricelist->{$id}->{exp}."): ".$pricelist->{$id}->{price}."\n";
